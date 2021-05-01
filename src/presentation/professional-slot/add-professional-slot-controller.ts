82105bc03eda98bd4b541c5261549c483ef756ad
Rodrigo <rodrigomafrarios@gmail.com>
2021-05-01 08:36:47 -0300

    feat: ensure AddProfessionalSlotController returns 400 if no start parameter provided

--- src/presentation/professional-slot/add-professional-slot-controller.ts
+++ src/presentation/professional-slot/add-professional-slot-controller.ts
@@ -1,7 +1,8 @@
 export class AddProfessionalSlotController {
   async handle (httpRequest: any): Promise<any> {
     return {
-      statusCode: 400
+      statusCode: 400,
+      body: new Error('Missing param: start')
     }
   }
 }
--- tests/presentation/professional-slot/add-professional-slot-controller.spec.ts
+++ tests/presentation/professional-slot/add-professional-slot-controller.spec.ts
@@ -13,4 +13,18 @@ describe('AddProfessionalSlotController', () => {
     const httpResponse = await controller.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
   })
+
+  test('Should return 400 if no start parameter provided', async () => {
+    const controller = new AddProfessionalSlotController()
+    const httpRequest = {
+      body: {
+        professionalId: 'any-professional-id',
+        end: new Date().toISOString()
+      }
+    }
+
+    const httpResponse = await controller.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: start'))
+  })
 })
