5d321ab08e7cb45a57e5b3b39d6aa390a9e7e428
Rodrigo <rodrigomafrarios@gmail.com>
2021-05-02 08:38:00 -0300

    feat: check if professional availability slot already exists

--- src/infra/db/mongodb/professional-slot/professional-slot-mongo-repository.ts
+++ src/infra/db/mongodb/professional-slot/professional-slot-mongo-repository.ts
@@ -2,11 +2,38 @@ import { AddProfessionalSlotRepository } from '@/data/interfaces/db/professional
 import { ProfessionalSlot } from '@/domain/models/professional-slot'
 import { AddProfessionalSlotParams } from '@/domain/usecases/professional-slot/add-professional-slot/add-professional-slot'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { ObjectId } from 'mongodb'
 
 export class ProfessionalSlotMongoRepository implements AddProfessionalSlotRepository {
   async add (data: AddProfessionalSlotParams): Promise<ProfessionalSlot> {
+    const professionalSlot = await this.loadByProfessionalIdAndPeriod(data)
+    if (professionalSlot) {
+       return professionalSlot
+    }
+    data = Object.assign(data, { professionalId: new ObjectId(data.professionalId) })
     const collection = await MongoHelper.getCollection('professional-availability-slots')
-		const result = await collection.insertOne(data)
-		return result && MongoHelper.map(result.ops[0])
+    const result = await collection.insertOne(data)
+    return result && MongoHelper.map(result.ops[0]) 
+  }
+  async loadByProfessionalIdAndPeriod (data: AddProfessionalSlotParams): Promise<ProfessionalSlot> {
+    const collection = await MongoHelper.getCollection('professional-availability-slots')
+    const result = await collection.findOne({
+      professionalId: new ObjectId(data.professionalId),
+      $or: [
+        { 
+          start: {
+            $gt: data.start,
+            $lt: data.end
+          } 
+        },
+        {
+          end: {
+            $gt: data.start,
+            $lt: data.end
+          }
+        }
+      ]
+    })
+    return result
   }
 }
--- tests/infra/db/mongodb/add-professional-slot-repository.spec.ts
+++ tests/infra/db/mongodb/add-professional-slot-repository.spec.ts
@@ -1,9 +1,10 @@
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import { mockAddProfessionalSlotParams } from '@/tests/data/mocks/db-professional-slot'
-import { Collection } from 'mongodb'
+import { Collection, ObjectId } from 'mongodb'
 import { ProfessionalSlotMongoRepository } from '@/infra/db/mongodb/professional-slot/professional-slot-mongo-repository'
 
 let collection: Collection
+let professionalsCollection: Collection
 
 const makeSut = (): ProfessionalSlotMongoRepository => {
   return new ProfessionalSlotMongoRepository()
@@ -20,14 +21,22 @@ describe('AddProfessionalSlotMongoRepository', () => {
 	beforeEach(async () => {
 		collection = await MongoHelper.getCollection('professional-availability-slots')
 		await collection.deleteMany({})
+
+		professionalsCollection = await MongoHelper.getCollection('professionals')
+  	await professionalsCollection.deleteMany({})
 	})
   describe('add()', () => {
     test('Should add a professional availability slot', async () => {
       const sut = makeSut()
-      const slot = await sut.add(mockAddProfessionalSlotParams())
+			const professional = await professionalsCollection.insertOne({
+				name: 'Rodrigo Mafra',
+				telefone: '11999999999'
+			})
+			const params = Object.assign({}, mockAddProfessionalSlotParams(), { professionalId: professional.ops[0]._id })
+      const slot = await sut.add(params)
 			expect(slot).toBeTruthy()
 			expect(slot.id).toBeTruthy()
-			expect(slot.professionalId).toBe('any-professional-id')
+			expect(slot.professionalId).toBe(professional.ops[0]._id)
 			expect(slot.isAvailable).toBe(true)
     })
   })
