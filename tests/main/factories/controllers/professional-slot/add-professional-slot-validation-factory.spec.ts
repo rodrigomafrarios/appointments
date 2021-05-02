beb5181005e72fd4ae6eaa592b86960241b152d2
Rodrigo <rodrigomafrarios@gmail.com>
2021-05-02 09:50:07 -0300

    test: add test to AddProfessionalSlotValidation factory

---
+++ tests/main/factories/controllers/professional-slot/add-professional-slot-validation-factory.spec.ts
@@ -0,0 +1,22 @@
+import { makeAddProfessionalSlotValidation } from '@/main/factories/controllers/add-professional-slot/add-professional-slot-validation-factory'
+import { Validation } from '@/presentation/interfaces'
+import { ProfessionalIdValidation } from '@/presentation/helpers/validators/professional-id-validation'
+import { RequiredFieldValidation } from '@/presentation/helpers/validators/required-field-validation'
+import { SlotValidation } from '@/presentation/helpers/validators/slot-validation'
+import { ValidationComposite } from '@/presentation/helpers/validators/validation-composite'
+jest.mock('@/presentation/helpers/validators/validation-composite')
+
+describe('AddProfessionalSlotValidation Factory', () => {
+  test('Should call ValidationComposite with all validations', () => {
+		makeAddProfessionalSlotValidation()
+		const validations: Validation[] = []
+    for (const field of ['start', 'end']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+
+    validations.push(new ProfessionalIdValidation())
+    validations.push(new SlotValidation())
+
+		expect(ValidationComposite).toHaveBeenCalledWith(validations)
+	})
+})
