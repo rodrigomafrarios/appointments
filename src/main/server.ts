4046063766be36e4e8fb566c55ac3bee17dc9182
Rodrigo <rodrigomafrarios@gmail.com>
2021-05-05 09:33:35 -0300

    1.7.0

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,13 +1,30 @@
 import 'module-alias/register'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import env from './config/env'
+import { Collection } from 'mongodb'
+
+let professionalsCollection: Collection
+let professionalSlotsCollection: Collection
+let bookingsCollection: Collection
 
 MongoHelper.connect(env.mongoUrl)
 	.then(async () => {
-	const app = (await import('./config/app')).default
-	app.listen(env.port, () => {
-		console.log(`Server running on http://localhost:${env.port}`)
-	})
+		professionalsCollection = await MongoHelper.getCollection('professionals')
+		professionalSlotsCollection = await MongoHelper.getCollection('professional-availability-slots')
+		bookingsCollection = await MongoHelper.getCollection('bookings')
+		
+		await bookingsCollection.deleteMany({})
+  	await professionalSlotsCollection.deleteMany({})
+		await professionalsCollection.deleteMany({})
+		
+		await professionalsCollection.insertOne({
+			name: 'Rodrigo Mafra',
+			telefone: '14981744452'
+		})
+		const app = (await import('./config/app')).default
+		app.listen(env.port, () => {
+			console.log(`Server running on http://localhost:${env.port}`)
+		})
 })
 .catch((e) => {
 	console.error(e)
